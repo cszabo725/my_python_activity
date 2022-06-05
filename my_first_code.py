01b69ec47a91618c13b03d53f7733824bc29bbef
cszabo725 <36697473+cszabo725@users.noreply.github.com>
2022-06-05 11:01:43 +0100

    Add files via upload
    First code using real estate example data

---
+++ my_first_code.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+# this is my first line of code
+print("My Name is Carl")
+print("The date is June 5th 2022")
+
+# write a program to analyse real estate data
+a = 45000
+b = 23400
+c = 67000
+d = 34600
+e = 12900
+
+# print total sales from December 
+total = a + b + c + d + e
+print(total)
+
+# print average sales price for December 
+print (total / 5)
+
+# print property price for property C
+print(c - 6500)
+
+
+# In[ ]:
+
+
+
+
